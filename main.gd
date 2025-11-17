a92822e69261fb35dd5ea897218d3c1bb7fa4a1a
project516 <138796702+Project516@users.noreply.github.com>
2025-11-17 16:59:28 -0600

    nice gui

--- main.gd
+++ main.gd
@@ -15,15 +15,18 @@ func _process(delta: float) -> void:
 
 
 func game_over() -> void:
-	
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
 	pass # Replace with function body.
 
 func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
+	get_tree().call_group("mobs", "queue_free")
 	
 
 
@@ -51,6 +54,7 @@ func _on_mob_timer_timeout() -> void:
 
 func _on_score_timer_timeout() -> void:
 	score += 1
+	$HUD.update_score(score)
 	pass # Replace with function body.
 
 
