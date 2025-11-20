6d48faacfc4b7aaa9a56199aaa70a9b02808a131
Project516 <138796702+Project516@users.noreply.github.com>
2025-11-20 16:03:28 +0000

    remove mobs

--- main.gd
+++ main.gd
@@ -1,6 +1,5 @@
 extends Node
 
-@export var mob_scene: PackedScene
 var score
 
 # Called when the node enters the scene tree for the first time.
@@ -16,7 +15,6 @@ func _process(delta: float) -> void:
 
 func game_over() -> void:
 	$ScoreTimer.stop()
-	$MobTimer.stop()
 	$HUD.show_game_over()
 	pass # Replace with function body.
 
@@ -26,30 +24,7 @@ func new_game():
 	$StartTimer.start()
 	$HUD.update_score(score)
 	$HUD.show_message("Get Ready")
-	get_tree().call_group("mobs", "queue_free")
-	
 
-
-func _on_mob_timer_timeout() -> void:
-	
-	var mob = mob_scene.instantiate()
-	
-	var mob_spawn_location = $MobPath/MobSpawnLocation
-	mob_spawn_location.progress_ratio = randf()
-	
-	mob.position = mob_spawn_location.position
-	
-	var direction = mob_spawn_location.rotation + PI / 2
-	
-	direction += randf_range(-PI / 4, PI / 4)
-	mob.rotation = direction
-	
-	var velocity = Vector2(randf_range(150.0, 250.0), 0.0)
-	mob.linear_velocity = velocity.rotated(direction)
-	
-	add_child(mob)
-	
-	pass # Replace with function body.
 
 
 func _on_score_timer_timeout() -> void:
@@ -59,7 +34,6 @@ func _on_score_timer_timeout() -> void:
 
 
 func _on_start_timer_timeout() -> void:
-	$MobTimer.start()
 	$ScoreTimer.start()
 	
 	pass # Replace with function body.
