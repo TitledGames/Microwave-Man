8e77ab6c5966b2c9515255e7326a49d26b774380
Project516 <138796702+Project516@users.noreply.github.com>
2025-11-29 17:48:24 -0600

    coin gets collected

---
+++ coin.gd
@@ -0,0 +1,19 @@
+extends Area2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_body_entered(body):
+	# Check if the body overlapping the coin is actually the player
+	if body.is_in_group("player"):
+		print("Coin collected!") 
+		# This deletes the coin from the world
+		queue_free()
