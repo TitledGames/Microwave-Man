7e2f603c247fa9139c5bf2ec2de0aeab00879609
Project516 <138796702+Project516@users.noreply.github.com>
2025-11-30 14:55:13 -0600

    enemy edits

--- enemy.gd
+++ enemy.gd
@@ -4,10 +4,10 @@ extends RigidBody2D
 var velocity = Vector2(1, 0)
 
 func _physics_process(delta):
-    linear_velocity = velocity * speed
+	linear_velocity = velocity * speed
 
 func _on_body_entered(body):
-    if body.is_in_group("walls"):
-        velocity.x *= -1
-    if body.is_in_group("player"):
-        get_tree().root.get_node("Main").game_over()
+	if body.is_in_group("walls"):
+		velocity.x *= -1
+	if body.is_in_group("player"):
+		get_tree().root.get_node("Main").game_over()
