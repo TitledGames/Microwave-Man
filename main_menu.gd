600b2750aae48252ed68c3725e42399d584eea30
project516 <138796702+Project516@users.noreply.github.com>
2026-02-26 15:05:35 +0000

    fix main menu crash on web
    this is similar to the crash that happened on the flathub build of the game. the fix for the web version was to exclude the extention on web export, and update the main menu script to not do any discord integration if it detcts a web platform

--- main_menu.gd
+++ main_menu.gd
@@ -1,28 +1,26 @@
 extends Node2D
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	DiscordRPC.app_id = 1465471267183263922
-	DiscordRPC.details = "Play as a microwave!"
-	DiscordRPC.state = "Playing the game!"
-	DiscordRPC.large_image = "ground"
-	DiscordRPC.large_image_text = "Play now!"
-	DiscordRPC.small_image = "level"
-	DiscordRPC.small_image_text = "Playing Microwave-Man!"
-
-	DiscordRPC.start_timestamp = int(Time.get_unix_time_from_system())
-
-	DiscordRPC.refresh()
+	if not OS.has_feature("web"):
+		# Look for the Autoload dynamically so the web parser doesn't crash
+		var discord = get_node_or_null("/root/DiscordRPC")
+		
+		if discord != null:
+			discord.app_id = 1465471267183263922
+			discord.details = "Play as a microwave!"
+			discord.state = "Playing the game!"
+			discord.large_image = "ground"
+			discord.large_image_text = "Play now!"
+			discord.small_image = "level"
+			discord.small_image_text = "Playing Microwave-Man!"
+			discord.start_timestamp = int(Time.get_unix_time_from_system())
+			discord.refresh()
 
 func _on_quit_pressed() -> void:
-	pass # Replace with function body.
 	get_tree().quit()
 
 func _on_playgame_pressed() -> void:
-	pass # Replace with function body.
 	get_tree().change_scene_to_file("res://main.tscn")
 
 func _on_extras_pressed() -> void:
-	pass # Replace with function body.
 	get_tree().change_scene_to_file("res://about.tscn")
