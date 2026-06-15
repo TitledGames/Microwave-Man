5e5973079b2dfb8fdbe77f3694267a0c2ca32b0b
project516 <138796702+Project516@users.noreply.github.com>
2026-06-15 16:40:25 -0500

    redesign the title screen to preview the game

--- main_menu.gd
+++ main_menu.gd
@@ -5,6 +5,8 @@ func _ready() -> void:
 	if version != "":
 		$BuildLabel.text = version
 
+	_animate_menu()
+
 	if not OS.has_feature("web") and Engine.has_singleton("DiscordRPC"):
 		var discord_rpc = Engine.get_singleton("DiscordRPC")
 		discord_rpc.set("app_id", 1465471267183263922)
@@ -16,6 +18,19 @@ func _ready() -> void:
 		discord_rpc.set("small_image_text", "Playing Microwave-Man!")
 		discord_rpc.set("start_timestamp", int(Time.get_unix_time_from_system()))
 		discord_rpc.call("refresh")
+
+func _animate_menu() -> void:
+	# Spin the coins and bob the title so the menu feels alive.
+	for coin in [$Coin, $Coin2]:
+		var full_x = coin.scale.x
+		var spin = create_tween().set_loops()
+		spin.tween_property(coin, "scale:x", full_x * 0.15, 0.45).set_trans(Tween.TRANS_SINE)
+		spin.tween_property(coin, "scale:x", full_x, 0.45).set_trans(Tween.TRANS_SINE)
+
+	var base_y = $title.position.y
+	var bob = create_tween().set_loops()
+	bob.tween_property($title, "position:y", base_y - 8.0, 1.2).set_trans(Tween.TRANS_SINE)
+	bob.tween_property($title, "position:y", base_y, 1.2).set_trans(Tween.TRANS_SINE)
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
