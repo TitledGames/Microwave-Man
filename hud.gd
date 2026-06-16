a8734a19cd2ba89877f5ba8d8387dadd61cd0397
project516 <138796702+Project516@users.noreply.github.com>
2026-06-16 17:13:54 -0500

    coin counter (#80)

--- hud.gd
+++ hud.gd
@@ -2,41 +2,12 @@ extends CanvasLayer
 
 signal start_game
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+func update_coins(coins: int) -> void:
+	$CoinLabel.text = "Coins: " + str(coins)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-func show_message(text):
-	$Message.text = text
-	$Message.show()
-	$MessageTimer.start()
-
-func show_game_over():
-	show_message("Game Over")
-	await $MessageTimer.timeout
-	
-	$Message.text = "place holder text"
-	$Message.show()
-	
-	await get_tree().create_timer(1.0).timeout
-	$StartButton.show()
-	
-func update_score(score):
-	$ScoreLabel.text = str(score)
-
-
+func update_timer(seconds: float) -> void:
+	$TimerLabel.text = GameState.format_time(seconds)
 
 func _on_start_button_pressed() -> void:
 	$StartButton.hide()
 	start_game.emit()
-	pass # Replace with function body.
-
-
-func _on_message_timer_timeout() -> void:
-	$Message.hide()
-	pass # Replace with function body.
